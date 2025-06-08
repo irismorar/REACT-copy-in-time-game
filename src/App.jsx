581daaf505b5e657cf1d7434610fe753661f1587
Iris Adina Morar <morariris@gmail.com>
2025-06-08 12:43:54 +0300

    app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import emojis from "./emojis.js";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function selectRandomElement(arr) {
+  const indexElement = Math.floor(Math.random() * arr.length);
+  return arr[indexElement];
 }
 
-export default App
+export default function App() {
+  const selected = selectRandomElement(emojis);
+
+  const handleCopy = (event) => {
+    if (event.target.innerText !== selected.face) {
+      alert("Wrong emoji");
+    } else if (event.timeStamp > 5000) {
+      alert("Too long. Reset and try again!");
+    } else {
+      alert("🎊 Winner 🎊");
+    }
+  };
+
+  const handleReset = () => {
+    window.location.reload();
+  };
+
+  return (
+    <main>
+      <h1>{selected.face}</h1>
+      <p>Copy this emoji</p>
+      <ul>
+        {emojis.map(({ id, face }) => {
+          return (
+            <li key={id} onCopy={handleCopy}>
+              {face}
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={handleReset}>RESET</button>
+    </main>
+  );
+}
---
+++ src/emojis.js
@@ -0,0 +1,17 @@
+const emojis = [
+  {
+    id: "frown",
+    face: "🙁",
+  },
+  {
+    id: "mid",
+    face: "😐",
+  },
+  {
+    id: "smile",
+    face: "🙂",
+  },
+  { id: "disputable", face: "🤨" },
+];
+
+export default emojis;
